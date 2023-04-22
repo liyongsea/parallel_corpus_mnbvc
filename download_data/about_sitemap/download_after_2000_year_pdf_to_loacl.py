b65e95e82da9fa04d09f4e8f519df20ac323f33c
Ran <redamancy0326@gmail.com>
2023-04-22 13:01:01 +0800

    map

--- download_data/about_sitemap/download_after_2000_year_pdf_to_loacl.py
+++ download_data/about_sitemap/download_after_2000_year_pdf_to_loacl.py
@@ -7,26 +7,25 @@ import multiprocessing
 import argparse
 from pathlib import Path
 from datasets import load_dataset
+from functools import partial
+
 
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
 }
 
-DOWNLOAD_PDF_FILE_ROOT_PATH = None
-ERROR_URL_SAVE_PATH = None
-
-def save_error_url(write_text):
-    if os.path.isfile(ERROR_URL_SAVE_PATH):
+def save_error_url(write_text, loacl):
+    if os.path.isfile(loacl):
         mode = 'a'
     else:
         mode = 'w'
         
-    with open(ERROR_URL_SAVE_PATH, mode) as f:
+    with open(loacl, mode) as f:
          f.write(write_text + '\n')
 
 
-def send_get_pdf_request_with_retry(url, headers=HEADERS, retries=3, backoff_factor=0.5):
+def send_get_pdf_request_with_retry(url, erroe_file_local, headers=HEADERS, retries=3, backoff_factor=0.5):
     session = requests.Session()
 
     #  Create a retry mechanism with the given parameters
@@ -43,25 +42,47 @@ def send_get_pdf_request_with_retry(url, headers=HEADERS, retries=3, backoff_fac
         # Raise an exception if the status code is not successful
         response.raise_for_status()
     except Exception as e:
-        save_error_url(url + "\n")
+        save_error_url(url + "\n", erroe_file_local)
         return None
     
+    # PDF is binary, so it returns content
     return response.content
 
 
-def save_pdf_file(url_information):
-    file_svae_dir_full_path= f"{DOWNLOAD_PDF_FILE_ROOT_PATH}/{url_information['record']}"
+def save_pdf_file(url_information, file_save_dir, erroe_file_local):
+    print(url_information['record'])
+    """
+    Args:
+        url_information:
+            Example: { 
+                        "record": 211803, 
+                        "language": "ar", 
+                        "year_time": 1954, 
+                        "file_name": "A_RES_863%28IX%29-AR.pdf", 
+                        "url":, "https://digitallibrary.un.org/record/211803/files/A_RES_863%28IX%29-AR.pdf"
+                    }
+                    
+        file_save_dir: Directory where downloaded files are saved
+        erroe_file_local: Request URL error to save the local location of the file
+
+
+    Returns:
+        Sitemap text
+    """
+    file_svae_dir_full_path= f"{file_save_dir}/{url_information['record']}"
     Path(file_svae_dir_full_path).mkdir(parents=True, exist_ok=True)
 
+    # Use the last file name of the URL as the file name
     file_local = os.path.join(file_svae_dir_full_path, url_information['url'].split('/')[-1])
     if os.path.exists(file_local):
         return
 
-    html_text = send_get_pdf_request_with_retry(url_information["url"])
+    html_text = send_get_pdf_request_with_retry(url_information["url"], erroe_file_local)
    
     if html_text:
         with open(file_local, mode='wb') as f:
              f.write(html_text)
+
 
 
 
@@ -72,14 +93,12 @@ if __name__ == '__main__':
     parser.add_argument('--worker_thread', default=0, type=int, help='并行核数')
 
     args = parser.parse_args()
-    ERROR_URL_FILE_LOCAL = args.erroe_file_local
-    DOWNLOAD_PDF_FILE_ROOT_PATH = args.file_save_dir
 
     Path(args.file_save_dir).mkdir(parents=True, exist_ok=True)
 
     # 在这里统一使用hugging仓库的dataset作为下载
     dataset = load_dataset("ranWang/UN_PDF_RECORD_SET", split="2000year")
 
-    with multiprocessing.Pool(processes=args.worker_thread or multiprocessing.cpu_count()) as pool:
-        for _ in tqdm(pool.imap_unordered(save_pdf_file, dataset), total=len(dataset)):
-            pass
+    partial_process_row = partial(save_pdf_file, file_save_dir=args.file_save_dir, erroe_file_local=args.erroe_file_local)
+
+    dataset.map(partial_process_row)
