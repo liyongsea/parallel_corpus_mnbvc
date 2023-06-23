ca66ce50dcebbcea205ac4de5e7a0b3cf47f3135
Kaguya <kaguya@Kaguya.cn>
2023-06-23 10:19:09 +0800

    feat: add preprocess script

--- alignment/script/gpt_helper.py
+++ alignment/script/gpt_helper.py
@@ -1,4 +1,6 @@
 import datetime
+from difflib import SequenceMatcher
+import itertools
 import os
 import time
 import json
@@ -39,8 +41,13 @@ def get_and_cache_dataset():
     except:
         dataset = datasets.load_dataset('bot-yaya/UN_PDF_SUBSET_PREPROCESSED', split='train')
         dataset.save_to_disk(my_path())
-    dataset = dataset.select(range(500)).filter(lambda x: x['record'] not in exception_files)
+    dataset = dataset.select(range(500))
+    #.filter(lambda x: x['record'] not in exception_files)
     return dataset
+
+def make_banner(record: str) -> str:
+    divider = '=' * 10 + '\n'
+    return  divider + record + '\n' + divider
 
 ## web
 def use_proxy():
@@ -116,7 +123,7 @@ def request_gpt_segment(prompt: str):
 def clearup_output(raw_output_from_chatgpt: str) -> list[str]:
     return list(filter(lambda x: len(x.strip()), raw_output_from_chatgpt.splitlines()))
 
-def lcs_sequence_alignment(ibatch: list[str] | str, obatch: list[str] | str) -> Tuple[dict[int, set[int]], list[float], list[float]]:
+def lcs_sequence_alignment(ibatch: list[str] | str, obatch: list[str] | str, drop_th=0.6) -> Tuple[dict[int, set[int]], list[float], list[float]]:
     """将ibatch每行的单词用最长公共子序列对齐到obatch每行的单词中。
     
     Args:
@@ -190,7 +197,7 @@ def lcs_sequence_alignment(ibatch: list[str] | str, obatch: list[str] | str) -> 
     print(mapping)
     print('orate', orate)
     for p, i in enumerate(orate):
-        if i < 0.6:
+        if i < drop_th:
             if p in mapping:
                 mapping.pop(p)
 
@@ -459,12 +466,12 @@ def push_idx_to_hf():
         """临时代码，转换手标数据"""
         dir = input('directory contain manual segmentation output text files:')
         outputmap = {}
-        jmap = {}
+        # jmap = {}
         for i in os.listdir(dir):
             with open(cat(dir, i, 'output.txt'), 'r', encoding='utf-8') as f:
                 outputmap[i] = f.read()
-            with open(cat(dir, i, 'is_hard_line_break.json'), 'r', encoding='utf-8') as f:
-                jmap[i] = json.load(f)
+            # with open(cat(dir, i, 'is_hard_line_break.json'), 'r', encoding='utf-8') as f:
+                # jmap[i] = json.load(f)
         upload_pending = []
         for i in ds.filter(lambda x: x['record'] in outputmap):
             rec = i['record']
@@ -478,11 +485,11 @@ def push_idx_to_hf():
             upload_pending.append({'record': i['record'], 'raw_text': i['en'], 'is_hard_linebreak': br_rev})
         return upload_pending
     
-    upload_pending = convert_idx()
+    upload_pending = convert_manual()
     hf_tk = read_secret('HF_TOKEN')
     print('dataset length:', len(upload_pending))
     upload_pending = datasets.Dataset.from_list(upload_pending)
-    upload_pending.push_to_hub(repo_id='gpt_joined_en_paragraph', split='train', token=hf_tk)
+    upload_pending.push_to_hub(repo_id='human_joined_en_paragraph_19', split='train', token=hf_tk)
 
 def download_and_visualize():
     """
@@ -508,6 +515,250 @@ def download_and_visualize():
     datasets.load_dataset('bot-yaya/EN_PARAGRAPH_GPT_JOINED', split='train').map(dump_to_file)
     
 
+def translate_and_align():
+    """
+    translate and align
+    just a idea
+    ########################
+    ###UNDER CONSTRUCTION###
+    ########################
+    """
+    from transformers import pipeline
+    tr = pipeline("translation", model=f"Helsinki-NLP/opus-mt-zh-en", device='cuda:0')
+
+    hm = datasets.load_dataset('bot-yaya/human_joined_en_paragraph_19', split='train')
+    recs = {x['record'] for x in hm}
+
+
+    def trans(row):
+        z = row['zh']
+        tr_e = []
+        for zl in z.splitlines():
+            tr_e.append(tr(zl)[0]['translation_text'].lower())
+        
+        br_rev = hm.filter(lambda x: x['record'] == row['record'])[0]['is_hard_linebreak']
+
+        buf = []
+        for p, i in enumerate(row['en'].lower().splitlines()):
+            if p == 0:
+                buf.append(i)
+            elif br_rev[p - 1]:
+                buf.append(i)
+            else:
+                buf[-1] += ' ' + i
+            
+        align_map, irate, orate = lcs_sequence_alignment(tr_e, buf, 0.4)
+        print(align_map)
+        print(irate)
+        print(orate)
+
+    (
+        datasets.load_dataset('bot-yaya/UN_PDF_SUBSET_PREPROCESSED', split='train')
+        .filter(lambda x: x['record'] in recs)
+        .map(trans)
+    )
+
+def preprocess_and_upload_dataset():
+    from collections import Counter
+    LANGS = ['zh', 'fr', 'es', 'ru', 'en']
+    PAGINATION_TOKEN = '\n----\n'
+    PREPROCESS_DIR = 'preprocessed_dump'
+    HEADER_SCAN_LIMIT = 100
+    DIGITS_PATTERN = re.compile('^\d+$')
+    MATCHER_RATIO = 0.72
+    FILTER_LOG = 'preprocessed_log.jsonl'
+
+    def make_filter_log(filtered: str, record: str | int, lang: str, page: str | int, reason: str):
+        """将过滤的内容写到log里方便分析"""
+        with open(my_path(FILTER_LOG), 'a', encoding='utf-8') as f:
+            json.dump({'record': str(record), 'lang': lang, 'page': str(page), 'reason': reason, 'filtered': filtered}, f)
+            f.write('\n')
+
+    def drop_pagination_header_and_footer(row: DatasetDict):
+        """语种无关过滤页眉（包括页码），不依赖任何正则，仅依靠自身和其它语种中出现的文本块频度统计实现
+
+        Args：
+            row (DatasetDict): datasets map进来的行，内含一篇文章的六个语种版本，每页用\n----\n隔开
+        Returns:
+            row (DatasetDict): 清洗后按原格式组装的row
+        """
+        record = row['record']
+
+        file_content = {}
+        token_spots = {}
+        line_spots = {}
+        page_token_slotses = {}
+
+        overall_token_spots = Counter()
+        overall_pages_num = 0
+
+        # maxpage = 0
+        for lang in LANGS:
+            file_content[lang] = pages = row[lang].split(PAGINATION_TOKEN)
+            overall_pages_num += len(pages)
+        #     maxpage = max(maxpage, len(pages))
+            token_spots[lang] = token_spot = Counter()  # token计数表，只用来完全匹配，其中页码特判
+            # 行计数表，比token粒度更大，用于difflib的近似匹配
+            line_spots[lang] = line_spot = Counter()
+            page_token_slotses[lang] = page_token_slots = [
+                set() for _ in pages]  # 每个用来装页眉的token，仅用来判断疑似页码的数字
+
+            for pageid, page in enumerate(pages):
+                lines = page.strip().splitlines()
+                page = pages[pageid] = '\n'.join(lines)
+
+                # 页眉只最多取前100字符
+                for lineid, line in enumerate(page[:HEADER_SCAN_LIMIT].splitlines()):
+                    for token in line.split():
+                        # if len(token) < 2: # 单字符的token太危险，不能要
+                        # continue
+                        page_token_slots[pageid].add(token)
+                        token_spot[token] += 1
+                    line_digest = line.replace(' ', '')
+                    if line_digest:
+                        # 行计数表是用于尝试清除类似P a g e 2这种形式的页码
+                        line_spot[line_digest] += 1
+
+            for token, ctr in token_spot.items():
+                overall_token_spots[token] += ctr
+
+            # 去掉只出现少数的token，提高效率
+            for x in list(token_spot.keys()):
+                if token_spot[x] < 3 or token_spot[x] > len(pages):
+                    token_spot.pop(x)
+
+        for lang, pages in file_content.items():
+            token_spot = token_spots[lang]
+            line_spot = line_spots[lang]
+            page_token_slots = page_token_slotses[lang]
+
+            pagination_offset = 1
+            maxcombo = 0
+            for offset in range(-9, 3):  # 0 1 2
+                combo = 0
+                for pageid in range(len(pages)):
+                    if str(pageid + offset) in page_token_slots[pageid]:
+                        combo += 1
+                if combo > maxcombo:
+                    maxcombo = combo
+                    pagination_offset = offset
+            # if maxcombo < len(pages) // 2:
+            #     pagination_offset = None
+
+            def is_freq(freq: int): return len(pages) >= 3 and freq >= len(
+                pages) - 1 or len(pages) >= 5 and freq > len(pages) * 2 / 3
+
+            for pageid, page in enumerate(pages):
+                header, body = page[:HEADER_SCAN_LIMIT], page[HEADER_SCAN_LIMIT:]
+                newlines = []
+                done = False  # 我们只删连续一段开头的，这样写来防止删掉类似the la de这些常见单词
+
+                for line in header.splitlines():
+                    # if 'A/CN.9/WG.VI/WP.22/Add.1' in line and lang == 'zh':
+                    #     print('break')
+                    # else:
+                    #     continue
+                    # # 行近似匹配
+                    line = line.strip()
+                    if not line or done:  # 空行不管，先照旧插入newlines
+                        # if line: # 这行用来管空行，即丢掉空行
+                        newlines.append(line)
+                        continue
+
+                    line_digest = line.replace(' ', '')
+
+                    # substr_score = Counter() # LCS得分，用于处理最长公共子序列，情况不多且过于复杂，先不用，这里留个想法
+                    line_freq = 0
+                    for line_str, ctr in line_spot.items():
+                        matcher = SequenceMatcher(
+                            None, line_digest, line_str, autojunk=False)
+                        # 上界剪枝
+                        if matcher.real_quick_ratio() > MATCHER_RATIO and matcher.quick_ratio() > MATCHER_RATIO and matcher.ratio() > MATCHER_RATIO:
+                            line_freq += ctr
+                    if is_freq(line_freq):
+                        make_filter_log(line, record, lang, pageid,
+                                        f'line_freq: {line_freq}, pages: {len(pages)}')
+                        continue
+                    # 按token过滤
+                    new_tokens = []
+                    for token in line.split(' '):
+                        # token.isdigit() 不可靠
+                        if not token:
+                            continue
+                        if not done:
+                            # 特判页码
+                            if pagination_offset is not None and re.match(DIGITS_PATTERN, token) and int(token) == pageid + pagination_offset:
+                                make_filter_log(token, record, lang,
+                                                pageid, f'likely page number')
+                                continue
+                            overall_token_freq = overall_token_spots[token]
+                            if overall_token_freq > overall_pages_num // 2:
+                                make_filter_log(
+                                    token, record, lang, pageid, f'overall_tk_freq: {overall_token_freq}, all_pages: {overall_pages_num}')
+                                continue
+                            # for token_str, ctr in token_spot.items():
+                                # if token_str == token:
+                                # token_freq += ctr
+                            token_freq = token_spot[token]
+                            if is_freq(token_freq) and not token_freq > len(pages):
+                                make_filter_log(
+                                    token, record, lang, pageid, f'tk_freq: {token_freq}, pages: {len(pages)}')
+                                continue
+
+                        new_tokens.append(token)
+                        done = True
+
+                    newlines.append(' '.join(new_tokens))
+
+                # 去页脚逻辑
+                annotation_index = body.find('__________')
+                if annotation_index != -1:
+                    make_filter_log(body[annotation_index:],
+                                    record, lang, pageid, f"annotation block")
+                    body = body[:annotation_index]
+
+                pages[pageid] = ('\n'.join(newlines) + body).strip()
+            row[lang] = PAGINATION_TOKEN.join(pages)  # 放回row，统一格式，之后用别的函数处理合页与成段
+
+    def remove_duplicate_breakline(pages: list[str]):
+        flatten = list(line.strip() for line in itertools.chain(*[page.splitlines() for page in pages]))
+        outputs = []
+        for i in flatten:
+            if not i:
+                continue
+            outputs.append(i)
+        return '\n'.join(outputs)
+
+    def chk_en_rate(row):
+        inputs = row['en']
+        en_rate = len(re.findall(r'[a-zA-Z]', inputs)) / len(inputs) if len(inputs) else 0
+        if en_rate < 0.2:
+            return False
+        return True
+
+    def dump_row(row):
+        """调试用，输出中间结果到文件，row是map的DatasetDict"""
+        for lang in LANGS:
+            with open(my_path(PREPROCESS_DIR, f'dbg_{lang}.txt'), 'a', encoding='utf-8') as f:
+                f.write(make_banner(row['record']) + row[lang])
+
+
+    def preprocess(row):
+        drop_pagination_header_and_footer(row)
+        for lang in LANGS:
+            row[lang] = remove_duplicate_breakline(row[lang].split(PAGINATION_TOKEN))
+        return row
+
+    use_proxy()
+    dataset = datasets.load_dataset("ranWang/un_pdf_text_data_test", split='randomTest10000')
+    dataset = dataset.filter(chk_en_rate).map(preprocess, num_proc=8)
+    dataset.map(dump_row)
+    dataset.save_to_disk(my_path())
+    print(len(dataset))
+    dataset.push_to_hub('bot-yaya/un_pdf_random10032_preprocessed2', token=read_secret('HF_TOKEN'))
+
+
+
 if __name__ == "__main__":
 
     if os.path.exists(my_path('chatgptexception.jsonl')):
@@ -528,6 +779,8 @@ if __name__ == "__main__":
         ('3', convert_output_text_to_idx),
         ('4', push_idx_to_hf),
         ('5', download_and_visualize),
+        ('6', translate_and_align),
+        ('7', preprocess_and_upload_dataset),
     ))
     while (cmd := input('''
     1: ask_gpt
@@ -535,6 +788,8 @@ if __name__ == "__main__":
     3: convert_output_text_to_idx
     4: push_idx_to_hf
     5: download_and_visualize
+    6: translate_and_align
+    7: preprocess_and_upload_dataset
 >>>''')
     ) not in command_map:
         print('invalid input')
