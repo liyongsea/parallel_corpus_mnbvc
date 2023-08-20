61f00e8747d13dce353e26a9e66346651b28de97
CHENG <chenghexuanfr@gmail.com>
2023-08-20 15:38:20 +0200

    download-UN-files V2 能跑了

--- download_data/doc_and_wpf/downloadV2.py
+++ download_data/doc_and_wpf/downloadV2.py
@@ -3,49 +3,53 @@
 2.自动获取cookie（requests）
 3.下载doc文件
 """
-
-
+import datadict as datadict
 import pandas as pd
 import requests as re
+import queue as  qu
+import time
 import os
 from datasets import load_dataset
 
-list=input("输入链接列表路径：")
-save=input("输入文件存储路径：")
+# 'dabaisuv/UN_Documents_2000_2023'
+def g_dict(link):#————取字典最后一列————
+    dataset = load_dataset(link)
+    dataset_n = dataset["train"]['链接']
+    return(dataset_n)
+def g_url(dset_n,n):#————取url————
+    res = dset_n[n]
+    return(res)
+def g_cookie():#————自动获取cookie————
+    sesObject = re.session()#创建session对象以保持cookie
+    reqRes = sesObject.post(url="https://documents.un.org/prod/ods.nsf/home.xsp")#进入主页面获取cookie
+    cook = sesObject.cookies
+    return(cook)
+def d_file(urln,cook,savePath):#————下载单个文档————
+        ju = urln[-3:]
+        nameFile = urln[-12:]
+        namePath = os.path.join(savePath, nameFile)
 
-"""——————1.获取Excel中文件个数——————"""
-df = pd.read_excel(list,sheet_name='Sheet1')
-row = df.shape[0]#得到行数（文件中加入表头）
+        res = re.get(urln, cookies=cook)
+        with open(namePath, 'wb') as f:
+            f.write(res.content)#写入doc
+        print("当前url:", urln, "下载完成",'\n')
+        return
 
-"""——————2.自动获取cookie——————"""
-sesObject = re.session()#创建session对象以保持cookie
-reqRes = sesObject.post(url="https://documents.un.org/prod/ods.nsf/home.xsp")#进入主页面获取cookie
-cook = sesObject.cookies
-print("文件总数；",row,", 下载开始：",'\n')
+if __name__ == '__main__':
 
-"""——————3.循环识别，判断是否为doc并下载——————"""
-i = 0# i为当前行
-j = 0# j为下载数
-while i < row:
-    urln = df.iloc[i, 2]  # 当前行文件（文件中加入表头）
-    # 识别url，文件格式以及文件全名
-    print("当前url:", urln)
-    ju = urln[-3:]
-    nameFile = urln[-12:]
+    link = 'dabaisuv/UN_Documents_2000_2023'
 
-    # 下载网页内容
-    res = re.get(urln, cookies=cook)
-    if res.status_code == 200:
-        print("网页请求成功")
+    d_set = g_dict(link)
+    num_row = len(d_set)
 
-    namePath = os.path.join(save, nameFile)
-    print(namePath)
+    save = input("输入文件存储路径：")
 
-    with open(namePath, 'wb') as f:
-        f.write(res.content)#写入doc
-        j=j+1
+    cookie_n=g_cookie()
 
-    print(nameFile, "下载完成",'\n')
-    i = i + 1
+    i = 0  # i为当前行
+    while i < num_row:                           #测试数量20,实用替换为num_row
+        urln = g_url(d_set, i)
+        d_file(urln,cookie_n,save)
+        i=i+1
 
-print("下载结束,文件总数：",row,"个,共下载",j,"个文件,存储位置",save)
+    print("下载结束,文件总数：",num_row,"个,共下载",i,"个文件,存储位置",save)
