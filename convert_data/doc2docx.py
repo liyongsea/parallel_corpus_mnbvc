ab7a8a8205bb01b4a55b74585cead98c83f544ee
Kaguya <kaguya@Kaguya.cn>
2023-09-17 16:15:09 +0800

    optimize performance

--- convert_data/doc2docx.py
+++ convert_data/doc2docx.py
@@ -21,6 +21,7 @@ def saved_path(file_abs): return os.path.join(SAVED_DIR, re.sub(r'\.\w+$', '.doc
 # @timeout_decorator.timeout(10)
 def save_as_docx(file_location):
     doc = None
+    # word = None
     try:
         # 获取文件的绝对路径，在Windows下调用客户端时必须使用绝对路径，否则无法打开
         file_abs = os.path.abspath(file_location)
@@ -37,7 +38,7 @@ def save_as_docx(file_location):
             return
 
         # 创建一个用于打开Word文档的客户端对象，需要下载WPS或者Office(Microsoft Word)
-        word = win32.gencache.EnsureDispatch('Word.Application')
+        word = win32.gencache.Dispatch('Word.Application')
 
         # 打开指定路径的文档
         doc = word.Documents.Open(file_abs)
@@ -50,27 +51,31 @@ def save_as_docx(file_location):
         # 关闭打开的文档资源
         doc.Close(False)
         doc = None
+        # word.Quit()
+        # word = None
     except:
         with open(err_path(file_abs), 'w') as f:
             pass
         if doc is not None:
             doc.Close(False)
+        # if word is not None:
+            # word.Quit()
 
 
 
-def task_wrapper(q1: mp.Queue):
-    while 1:
-        absfn = q1.get()
-        if absfn is None:
-            return
-        proc = mp.Process(target=save_as_docx, args=(absfn,))
-        proc.start()
-        proc.join(timeout=10)
-        if proc.is_alive():
-            proc.terminate()
-            proc.join()
-            with open(err_path(absfn), 'w') as f:
-                pass
+# def task_wrapper(q1: mp.Queue):
+#     while 1:
+#         absfn = q1.get()
+#         if absfn is None:
+#             return
+#         proc = mp.Process(target=save_as_docx, args=(absfn,))
+#         proc.start()
+#         proc.join(timeout=10)
+#         if proc.is_alive():
+#             proc.terminate()
+#             proc.join()
+#             with open(err_path(absfn), 'w') as f:
+#                 pass
         # save_as_docx(absfn)
 
 if __name__ == '__main__':
@@ -109,12 +114,12 @@ if __name__ == '__main__':
                         proc.join()
                         with open(err_path(absfn), 'w') as f:
                             pass
-                    for process in psutil.process_iter(attrs=['pid', 'name']):
-                        if process.info['name'] == "WINWORD.EXE":
-                            pid = process.info['pid']
-                            p = psutil.Process(pid)
-                            # p.terminate()  # 终止进程
-                            p.kill()
-                            print('killed', pid, process.info['name'])
+                        for process in psutil.process_iter(attrs=['pid', 'name']):
+                            if process.info['name'] == "WINWORD.EXE":
+                                pid = process.info['pid']
+                                p = psutil.Process(pid)
+                                # p.terminate()  # 终止进程
+                                p.kill()
+                                print('killed', pid, process.info['name'])
     # for _ in consumers: q1.put(None)
     # for x in consumers: x.join()
