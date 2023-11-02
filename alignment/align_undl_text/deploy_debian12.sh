2b505d802149dc43a971d47b57e08bbacae6a0e1
Kaguya <kaguya@Kaguya.cn>
2023-11-02 21:41:35 +0800

    update deploy script

--- alignment/align_undl_text/deploy_debian12.sh
+++ alignment/align_undl_text/deploy_debian12.sh
@@ -2,7 +2,5 @@ apt install -y python3-pip python3-full
 python3 -m venv MT
 cd MT
 wget https://raw.githubusercontent.com/liyongsea/parallel_corpus_mnbvc/doc2docx/alignment/align_undl_text/tr_client_argostranslate.py
-wget https://raw.githubusercontent.com/liyongsea/parallel_corpus_mnbvc/doc2docx/alignment/align_undl_text/tr_install_argostranslate.py
 bin/pip install argostranslate requests
-bin/python tr_install_argostranslate.py
 nohup bin/python tr_client_argostranslate.py &
