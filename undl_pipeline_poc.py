c5e5699436b4e1b82b052c4dfb19190f79cffeb0
ATRI <A@T.RI>
2024-01-03 11:51:52 +0800

    pipeline的基本框架，打算做成单文件

---
+++ undl_pipeline_poc.py
@@ -0,0 +1,39 @@
+"""
+本代码用于演示我们是如何从数据源进行一步步操作从而得到数据集
+
+出于简洁明了的目的，不会针对各个环节的效率进行优化
+
+实际实践所用代码可以参照https://wiki.mnbvc.org/doku.php/pxyl中留档的代码
+"""
+
+
+
+def download_doc_from_net():
+    """
+    数据源下载，经调研，发现https://documents.un.org/prod/ods.nsf/home.xsp内含有doc格式文件
+    """
+
+def doc2docx():
+    """
+    将doc格式文件转换为docx格式文件。
+    """
+
+def wpf2docx():
+    """
+    将wpf格式文件转换为docx格式文件。
+    """
+
+def docx2txt():
+    """
+    将docx格式文件转换为txt格式文件。
+    """
+
+def translate():
+    """
+    文本翻译
+    """
+
+def align():
+    """
+    文本对齐
+    """
