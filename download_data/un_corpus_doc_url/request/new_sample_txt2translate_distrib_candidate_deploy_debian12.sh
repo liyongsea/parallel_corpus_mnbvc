c247df88c5bcdd7ff6f31f394048617d9c8b3dc4
Kaguya <kaguya@Kaguya.cn>
2024-09-27 13:58:31 +0800

    脚本换成两步

--- download_data/un_corpus_doc_url/request/new_sample_txt2translate_distrib_candidate_deploy_debian12.sh
+++ download_data/un_corpus_doc_url/request/new_sample_txt2translate_distrib_candidate_deploy_debian12.sh
@@ -10,11 +10,11 @@ nohup bin/python new_sample_txt2translate_distrib_candidate_client.py &
 
 
 # google cloud shell
-
-apt update
-apt install -y python3-pip python3-full screen
 cd /root
-pip3 install argostranslate requests
+wget https://raw.githubusercontent.com/liyongsea/parallel_corpus_mnbvc/refs/heads/pipeline_rework/download_data/un_corpus_doc_url/request/new_sample_txt2translate_distrib_candidate_client.py
 # 改地址
 nano new_sample_txt2translate_distrib_candidate_client.py
+apt update
+apt install -y python3-pip python3-full screen
+pip3 install argostranslate requests
 screen -dmS mt bash -c 'python3 new_sample_txt2translate_distrib_candidate_client.py'
