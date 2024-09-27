c2355cc42dd5e092cd26ae0178c04792c77f56f3
Kaguya <kaguya@Kaguya.cn>
2024-09-27 11:06:59 +0800

    google cloud shell用脚本

--- download_data/un_corpus_doc_url/request/new_sample_txt2translate_distrib_candidate_deploy_debian12.sh
+++ download_data/un_corpus_doc_url/request/new_sample_txt2translate_distrib_candidate_deploy_debian12.sh
@@ -4,4 +4,17 @@ python3 -m venv MT
 cd MT
 wget https://raw.githubusercontent.com/liyongsea/parallel_corpus_mnbvc/refs/heads/pipeline_rework/download_data/un_corpus_doc_url/request/new_sample_txt2translate_distrib_candidate_client.py
 bin/pip install argostranslate requests
+# 改地址
+nano new_sample_txt2translate_distrib_candidate_client.py
 nohup bin/python new_sample_txt2translate_distrib_candidate_client.py &
+
+
+# google cloud shell
+
+apt update
+apt install -y python3-pip python3-full screen
+cd /root
+pip3 install argostranslate requests
+# 改地址
+nano new_sample_txt2translate_distrib_candidate_client.py
+screen -dmS mt bash -c 'python3 new_sample_txt2translate_distrib_candidate_client.py'
